aeb0229cf8d890b327ea73c75285552d24b56e5d
BurdujaAdrian <105036513+BurdujaAdrian@users.noreply.github.com>
2025-05-01 13:01:48 +0300

    Update burduja_adrian_impermanent_relatilation_round_2.py

--- burduja_adrian_impermanent_relatilation_round_2.py
+++ burduja_adrian_impermanent_relatilation_round_2.py
@@ -4,14 +4,14 @@ def strategy_round_2(opponent_id:int,my_history: dict[int,list[int]], opponents_
     func = lambda seed : (1664525*seed + 1013904223)%2**32
     curr_hist = my_history[opponent_id]
     opponent_history = opponents_history[opponent_id]
-
+    seed = 0
     response = 1
 
     lenl = len(curr_hist)-1
     if lenl >= 0:
 
         if curr_hist[lenl] == 0 or opponent_history[lenl] == 0:
-            seed = 0
+           
             for n in range(lenl):
                 seed += (2**n) * curr_hist[n]
             forgive = func(seed) % 100 > 80
@@ -20,7 +20,7 @@ def strategy_round_2(opponent_id:int,my_history: dict[int,list[int]], opponents_
 
     # if it's the last possible game against this opponent, defect
     if lenl == 198 :
-        respone = 0
+        response = 0
 
     next_op = func(seed) % len(opponents_history.keys())
 
@@ -31,5 +31,5 @@ def strategy_round_2(opponent_id:int,my_history: dict[int,list[int]], opponents_
         if min_def > defections and len(opponets_history[key]) < 200:
             next_op = key
 
-    return tuple[next_op,response]
+    return (next_op,response)
 
